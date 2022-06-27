f42c940f6fb28502e9928e79e626b7f9e5b122d4
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-06-27 14:54:13 +0300

    feat:firstTest

--- src/test/java/MyFirstChromeTest.java
+++ src/test/java/MyFirstChromeTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MyFirstChromeTest {
+import org.junit.Assert;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstChromeTest {
+    @Test
+    public void startWebDriver(){
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("http://seleniumsimplified.com");
+       Assert.assertTrue("title should start differently",
+               webDriver.getTitle().startsWith("Selenium Simplified"));
+        webDriver.close();
+        webDriver.quit();
+    }
 }
